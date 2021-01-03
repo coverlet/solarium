d85de0ffe701d8a5102146bb0359f34bc29bf927
maricalucian <maricalucian@gmail.com>
2021-01-03 14:42:16 +0200

    Add a custom font

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -19,6 +19,11 @@ export const DefaultLayout = ({
     <>
       <Head>
         <title>{title}</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Container className={styles['main-container']}>
         <SolHeader />
