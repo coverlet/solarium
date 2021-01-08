974afe3f1389ec60601d554bbbc307e8f958e577
Cristi Calacianu <cristi_calacianu@yahoo.com>
2021-01-08 22:43:38 +0200

    added unit tests for footer

---
+++ src/components/sol-footer/__test__/sol-footer.test.tsx
@@ -0,0 +1,21 @@
+import { cleanup, render, screen } from '@testing-library/react';
+import { SolFooter } from '../sol-footer';
+
+// more info
+// https://testing-library.com/docs/react-testing-library/example-intro
+// https://www.leighhalliday.com/introduction-react-testing-library
+
+afterEach(cleanup);
+
+describe('footer components', () => {
+  it('renders', () => {
+    const { asFragment } = render(<SolFooter />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('contains text', () => {
+    render(<SolFooter />);
+    // screen.debug();
+    expect(screen.getByText(/© 2021/i)).toBeInTheDocument();
+  });
+});
